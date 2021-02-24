4595a7475080c01f6c8f928756bb647a0aaff956
Sindi <sindidaconceicao@gmail.com>
2021-02-24 19:14:31 -0400

    agregue estilos personalizados a react-select

--- components/shared/ListContainer.js
+++ components/shared/ListContainer.js
@@ -5,15 +5,65 @@ import CardVideo from './CardVideo'
 import Select from 'react-select';
 
 const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' }
-  ]
+    { value: 'Categoría1', label: 'Categoría 1' },
+    { value: 'Categoría2', label: 'Categoría 2' },
+    { value: 'Categoría3', label: 'Categoría 3' }
+]
+
+const colourStyles = {
+    control: (styles, { isFocused, isSelected }) => ({
+        ...styles, 
+        backgroundColor: 'var(--blue)', 
+        border: 'solid 2px var(--violet)', 
+        borderRadius: 0,
+        boxShadow: isFocused ? 'none' : null,
+        ':hover': {
+            ...styles[':active'],
+            border: 'solid 2px var(--violet)',
+            cursor: 'pointer',
+          },
+    }),
+    valueContainer: styles => ({
+        ...styles,
+        justifyContent: 'center',
+        fontWeight: 'bold',
+        padding: '0px 0px 0px 36px'
+    }),
+    indicatorSeparator: styles => ({
+        ...styles,
+        display: 'none',
+    }),
+    menu: styles => ({
+        ...styles,
+        borderRadius: 0,
+        border: 'solid 2px var(--violet)',
+        top: '30px',
+        boxShadow: 'none',
+    }),
+    menuList: styles => ({
+        ...styles,
+        padding: '0px',
+    }),
+    option: (styles, { isFocused, isSelected }) => {
+        return {
+          ...styles,
+          backgroundColor: isSelected
+            ? 'var(--violet)'
+            : isFocused
+            ? 'var(--violet)'
+            : 'var(--blue)',
+          color: 'white',
+          cursor: isSelected ? 'default' : 'pointer',
+          borderBottom: 'solid 1px var(--violet)',
+          textAlign: 'center',
+        };
+    },
+  };
 
 const ListContainer = () => (
     <div className={styles.container}>
         <div className={styles.select}>
-          <Select options={options} />
+          <Select options={options} styles={colourStyles}/>
         </div>
         <div className={styles.list}>
             <CardVideo/>
