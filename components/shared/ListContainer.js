31230d2b87aa87ebd22073e3f46e21696ce92600
Sindi <sindidaconceicao@gmail.com>
2021-02-24 16:20:13 -0400

    agregue select-react con su estilo pre-determinado

--- components/shared/ListContainer.js
+++ components/shared/ListContainer.js
@@ -2,17 +2,18 @@ import Link from 'next/link'
 import styles from '../../styles/ListContainer.module.css'
 import CardVideo from './CardVideo'
 
+import Select from 'react-select';
+
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+
 const ListContainer = () => (
     <div className={styles.container}>
-        <div className={styles.select} tabindex="1">
-            <input name="test" type="radio" id="opt1" checked/>
-            <label for="opt1">Categoría 1</label>
-            <input name="test" type="radio" id="opt2"/>
-            <label for="opt2">Categoría 2</label>
-            <input name="test" type="radio" id="opt3"/>
-            <label for="opt3">Categoría 3</label>
-            <input name="test" type="radio" id="opt4"/>
-            <label for="opt4">Categoría 4</label>
+        <div className={styles.select}>
+          <Select options={options} />
         </div>
         <div className={styles.list}>
             <CardVideo/>
