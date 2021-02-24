147425aa661ec1f3462f403eaa9d0411b8c6d1e5
Joel Blanco <jblnco.ing@example.com>
2021-02-23 22:33:53 -0400

    - Se agrego el icono de play - Se reorganizo el espacio es los card video

--- components/shared/CardVideo.js
+++ components/shared/CardVideo.js
@@ -2,7 +2,10 @@ import styles from '../../styles/CardVideo.module.css'
 
 const CardVideo = () => (
     <div className={styles.container}>
-        <img className={styles.img} src="/assets/Video.png"/>
+        <div className={styles.img_container}>
+            <img className={styles.img} src="/assets/Video.png"/>
+            <img className={styles.play} src="/assets/play_arrow_24px.svg"/>
+        </div>
         <p className={styles.description}>testo del video</p>
     </div>
 )
