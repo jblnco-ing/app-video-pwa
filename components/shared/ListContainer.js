c68ac89c9cdd4b102ffa423a7a862f0efa928ca0
Sindi <sindidaconceicao@gmail.com>
2021-02-24 09:14:11 -0400

    hice la lista con label y agregue estilos

--- components/shared/ListContainer.js
+++ components/shared/ListContainer.js
@@ -4,11 +4,16 @@ import CardVideo from './CardVideo'
 
 const ListContainer = () => (
     <div className={styles.container}>
-            <select>
-                <option value="1" selected>
-                    Categoria 1
-            </option>
-            </select>
+        <div className={styles.select} tabindex="1">
+            <input name="test" type="radio" id="opt1" checked/>
+            <label for="opt1">Categoría 1</label>
+            <input name="test" type="radio" id="opt2"/>
+            <label for="opt2">Categoría 2</label>
+            <input name="test" type="radio" id="opt3"/>
+            <label for="opt3">Categoría 3</label>
+            <input name="test" type="radio" id="opt4"/>
+            <label for="opt4">Categoría 4</label>
+        </div>
         <div className={styles.list}>
             <CardVideo/>
             <CardVideo/>
