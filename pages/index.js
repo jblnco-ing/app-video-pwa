0aa56b35599e55a635e674355170d693fc0083f2
Joel Blanco <jblnco.ing@example.com>
2021-02-26 00:15:21 -0400

    - Se cambio el color del tema en las variables del head

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
         <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
         <link rel='apple-touch-icon' href='/apple-icon.png'></link>
-        <meta name='theme-color' content='#317EFB' />
+        <meta name='theme-color' content='#FCBBB2' />
         <link rel="icon" href="/favicon.ico" />
         <title>Next.js PWA Example</title>
       </Head>
