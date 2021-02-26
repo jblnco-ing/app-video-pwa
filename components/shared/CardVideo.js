2241cfa1faeb4a8a5a4f7e96cf08af14e90488f2
Joel Blanco <jblnco.ing@example.com>
2021-02-26 15:48:30 -0400

    - Se agrego un nuevo icono de play

--- components/shared/CardVideo.js
+++ components/shared/CardVideo.js
@@ -6,7 +6,7 @@ const CardVideo = (props) => (
         <Link href={`video/${props.id}`}>
             <div className={styles.img_container}>
                 <img className={styles.img} src={props.img} />
-                <img className={styles.play} src="/assets/play_arrow_24px.svg" />
+                <img className={styles.play} width="30" src="/assets/play-button.svg" />
             </div>
         </Link>
         <p className={styles.description}>{props.text}</p>
