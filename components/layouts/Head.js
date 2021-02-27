9d78a3936d6bf434b790b868882dc21cdd155990
Joel Blanco <jblnco.ing@example.com>
2021-02-26 21:05:36 -0400

    - Se agrego los splash para ios - Sigo probando el maskable

--- components/layouts/Head.js
+++ components/layouts/Head.js
@@ -1,20 +1,31 @@
 import HeadNext from "next/head";
 
-const Head = ()=>(
-<HeadNext>
-    <meta charset='utf-8' />
-    <meta http-equiv='X-UA-Compatible' content='IE=edge' />
-    <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
-    <meta name='description' content='Description' />
-    <meta name='keywords' content='Keywords' />
-    <link rel='manifest' href='/manifest.json' />
-    <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
-    <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
-    <link href="icons/icon-192x192.png" rel="icon" sizes="192x192"></link>
-    <link href="icons/icon-128x128.png" rel="icon" sizes="128x128"></link>
-    <link rel='apple-touch-icon' href='/apple-icon.png'></link>
-    <meta name='theme-color' content='#eeeeee' />
-    <link rel="icon" href="/favicon.ico" />
-    <title>Next.js PWA Example</title>
-</HeadNext>);
+const Head = () => (
+    <HeadNext>
+        <meta charset='utf-8' />
+        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+        <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+        <meta name='description' content='Description' />
+        <meta name='keywords' content='Keywords' />
+        <link rel='manifest' href='/manifest.json' />
+        <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
+        <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+        <link href="icons/icon-192x192.png" rel="icon" sizes="192x192"></link>
+        <link href="icons/icon-128x128.png" rel="icon" sizes="128x128"></link>
+        <meta name='theme-color' content='#eeeeee' />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="apple-mobile-web-app-capable" content="yes"/>
+        <link rel='apple-touch-icon' href='/apple-icon.png'></link>
+        <link href="splashscreens/iphone5_splash.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+        <link href="splashscreens/iphone6_splash.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+        <link href="splashscreens/iphoneplus_splash.png" media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+        <link href="splashscreens/iphonex_splash.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+        <link href="splashscreens/iphonexr_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+        <link href="splashscreens/iphonexsmax_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+        <link href="splashscreens/ipad_splash.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+        <link href="splashscreens/ipadpro1_splash.png" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+        <link href="splashscreens/ipadpro3_splash.png" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+        <link href="splashscreens/ipadpro2_splash.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+        <title>Next.js PWA Example</title>
+    </HeadNext>);
 export default Head;
